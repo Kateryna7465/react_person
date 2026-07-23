56fd9f4bc7301cd03de2b5453bf12cb4290ee515
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-07-23 13:32:45 +0200

    Create Person component and handle partner logic

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const partnerRole = person.sex === 'm' ? 'wife' : 'husband';
+
+  const ageElement = person.age && (
+    <p className="Person__age">Age: {person.age}</p>
+  );
+
+  const marriageStatus = person.isMarried
+    ? `${partnerRole}: ${person.PartnerName}`
+    : 'i am not married';
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{person.name}</h2>
+
+      {ageElement}
+
+      <p className="Person__partner">{marriageStatus}</p>
+    </div>
+  );
+};
